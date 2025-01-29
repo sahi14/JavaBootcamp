c9f95e543e49d84020c6e4bc478958a19764229a
unknown <surabhi.sahi@gmail.com>
2025-01-29 09:59:14 +0530

    Calculate Area

---
+++ parameters.java
@@ -0,0 +1,11 @@
+public class parameters {
+
+    public static void main(String[] args) {
+        calculateArea(2.3, 3.6);
+
+    }
+    public static void calculateArea(double length, double width) {
+        double area = length * width;
+        System.out.println("Area" + area);
+    }
+}
