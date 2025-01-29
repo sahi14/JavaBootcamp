52bf7a858950ac7c44f9739690799f28e2e9f452
unknown <surabhi.sahi@gmail.com>
2025-01-29 10:37:07 +0530

    Return Values

---
+++ ReturnValues.java
@@ -0,0 +1,12 @@
+public class ReturnValues {
+
+    public static void main(String[] args) {
+        calculateArea(2.3, 3.6);
+
+    }
+    public static double calculateArea(double length, double width) {
+        double area = length * width;
+        return area;
+    }
+    
+}
