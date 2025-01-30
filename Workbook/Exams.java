c2264aff60a7f59c4529d0c4903240ad53e22210
unknown <surabhi.sahi@gmail.com>
2025-01-30 10:14:02 +0530

    Arrays Workbook 6.1

---
+++ Workbook/Exams.java
@@ -0,0 +1,16 @@
+public class Exams {
+    public static void main(String[] args) {
+        
+        System.out.println("its time to take your 5th sem exams. Please, take your seats");
+        String[] seats = { "Harry", "Neville" , "Ron", "Hermione", "Seamus"};
+    
+        System.out.println(seats[0] + ", you will take seat " + 0);
+        System.out.println(seats[1] + ", you will take seat " + 1);
+        System.out.println(seats[2] + ", you will take seat " + 2);
+        System.out.println(seats[3] + ", you will take seat " + 3);
+        System.out.println(seats[4] + ", you will take seat " + 4);
+    
+    
+}
+    
+}
