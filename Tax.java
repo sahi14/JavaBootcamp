a335ad1dcae90ce46727932ecea2bf357085743d
unknown <surabhi.sahi@gmail.com>
2025-01-30 12:38:10 +0530

    Arrays Workbook 6.9

---
+++ Tax.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Tax {
+
+    public static void main(String[] args) {
+        double[] price = {1.99, 2.99, 3.99, 4.99};
+        double[] afterTax = new double[price.length];
+
+        for ( int i = 0; i < afterTax.length; i++) {
+            afterTax[i] = price[i] + price[i]*0.13;
+        }
+
+        System.out.println("The original prices are: " + Arrays.toString(price));
+        System.out.println("The prices after tax are: " +  Arrays.toString(afterTax));
+    }
+    
+}
