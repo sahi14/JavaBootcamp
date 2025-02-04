686ad9aef06ec3579fe4cb41e21acb9a7d6c40ca
unknown <surabhi.sahi@gmail.com>
2025-02-04 15:54:07 +0530

    WORKBOOK 10

---
+++ WORKBOOK10/Team.java
@@ -0,0 +1,37 @@
+package WORKBOOK10;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Team {
+
+    private String name;
+    private Map<String, String> players;
+
+    public Team(String name) {
+        this.name = name;
+        this.players = new HashMap<>();
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.isBlank())
+            throw new IllegalArgumentException("Name cannot be null/blank");
+        this.name = name;
+    }
+
+    public String getPlayer(String position) {
+        return this.players.get(position);
+    }
+
+    public void setPlayer(String position, String player) {
+        if (position == null)
+            throw new IllegalArgumentException("Position cannot be null");
+
+        this.players.put(position, player);
+    }
+    
+}
