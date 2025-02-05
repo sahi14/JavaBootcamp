e26d8885da05b4d355abf8d12eafe5056db4ef4c
unknown <surabhi.sahi@gmail.com>
2025-02-05 11:45:15 +0530

    Java Multithreading

---
+++ Multithreading/ThreadCreation.java
@@ -0,0 +1,19 @@
+package Multithreading;
+
+public class ThreadCreation {
+
+    // public static class ChildThreadTask implements Runnable {
+
+    // }
+
+    public static void main(String[] args) {
+        count();
+    }
+
+    public static void count() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+    }
+    
+}
