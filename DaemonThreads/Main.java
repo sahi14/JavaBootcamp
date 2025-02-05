3390f037474f61d398f0ed2ff3a2e7ea0f4f6d58
unknown <surabhi.sahi@gmail.com>
2025-02-05 12:24:31 +0530

    Daemon Threads

---
+++ DaemonThreads/Main.java
@@ -0,0 +1,13 @@
+package DaemonThreads;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Thread memoryMonitorThread = new Thread(new MemoryMonitorDaemon());
+        memoryMonitorThread.start();
+
+        Thread memoryIntensiveTaskThread = new Thread(new MemoryIntensiveTask());
+        memoryIntensiveTaskThread.start();
+    }
+    
+}
